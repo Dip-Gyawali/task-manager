277425adae62bf979073304f69d72be5772df447
Hiten <hitengamer11@gmail.com>
2024-10-17 23:31:52 +0545

    frontend added and backend operation done

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require('mongoose');
 const tasks = require('../model/taskModels')
 
 const getallTasks = async (req, res) => {
@@ -21,15 +22,54 @@ const addTask = async (req, res) => {
 };
 
 const getSingleTask = async (req, res) => {
-  res.status(200).json({ data: "get data by id" });
+  const {id} = req.params;
+  if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(400).json({error:"Invalid Id"})
+  }
+  try{
+    const task = await tasks.findById(id);
+    if(!task){
+      return res.status(400).json({error:`Cannot Find Task with ${id}`})
+    }
+    res.status(200).json({data:task})
+  }
+  catch(err){
+    res.status(400).json({error:"Cannot Find Task"})
+  }
 };
 
-const updateTask = (req, res) => {
-  res.status(200).json({ data: "update data" });
+const updateTask = async (req, res) => {
+  const {id} = req.params;
+  if(!mongoose.Types.ObjectId.isValid(id)){
+    res.status(400).json({error:"Invalid ID"})
+  }
+  try{
+    const updatedTask = await tasks.findOneAndUpdate({_id:id},{...req.body});
+    if(!updateTask){
+      return res.status(400).json({error:`Cannot Find Task with ${id}`})
+    }
+    res.status(200).json({data:updatedTask})
+  }
+  catch(err){
+    res.status(400).json({error:"Cannot Update"})
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.status(200).json({ data: "deleted" });
+const deleteTask = async (req, res) => {
+  const {id} = req.params;
+  if(!mongoose.Types.ObjectId.isValid(id)){
+    res.status(400).json({error:"Invalid Id"})
+  }
+  try{
+   const TaskDeleted = await tasks.findByIdAndDelete({_id:id});
+   if(!TaskDeleted){
+    return res.status(400).json({error:`Cannot Find Task with ${id}`})
+   }
+   res.status(200).json({data: TaskDeleted})
+  }
+  catch(err){
+    res.status(400).json({error: "Cannot Delete" });
+  }
 };
 
 module.exports = {
